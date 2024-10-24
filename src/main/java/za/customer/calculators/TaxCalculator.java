d21e52a37a3c470669061b9b88ddaa63ec274e34
nic0michael <nicomichael2012@gmail.com>
2024-10-24 09:32:36 +0200

    removed code for training purposes

--- src/main/java/za/customer/calculators/TaxCalculator.java
+++ src/main/java/za/customer/calculators/TaxCalculator.java
@@ -11,6 +11,9 @@ import za.customer.enums.Gender;
 import java.math.RoundingMode;
 import java.security.InvalidParameterException;
 
+/**
+  *  you need to use TDD to create this code
+  */
 @Component
 public class TaxCalculator {
     private final CustomerAdapter adapter;
@@ -22,73 +25,6 @@ public class TaxCalculator {
 
     public CustomerDto calculateTax(CustomerRequest customerRequest) throws InvalidParameterException {
 
-        CustomerDto dto;
-        try {
-            dto = adapter.convert(customerRequest);
-            int age = dto.getAge();
-            Gender gender = dto.getCustGender();
-            double income = dto.getIncome();
-            double taxFactor;
-            double incomeTax;
-            taxFactor = basicTaxBusinessRule(income);
-            taxFactor = taxFactor * ageBusinessRule(age);
-            taxFactor = taxFactor * genderBusinessRule(gender);
-            incomeTax = income * taxFactor;
-
-            BigDecimal bd = BigDecimal.valueOf(incomeTax);
-            bd = bd.setScale(2, RoundingMode.HALF_UP);
-            incomeTax = bd.doubleValue();
-
-            dto.setIncomeTax(incomeTax);
-
-        } catch (Exception e) {
-            throw new InvalidParameterException(e.getMessage());
-        }
-
-        return dto;
     }
-
-
-    private double basicTaxBusinessRule(double income) throws InvalidParameterException {
-        double theTaxFactor = 0;
-
-        if (income < 2000) {
-            theTaxFactor = 0.1;
-        } else if (income < 10000) {
-            theTaxFactor = 0.75;
-        } else if (income < 50000) {
-            theTaxFactor = 0.80;
-        } else{
-            theTaxFactor = 0.90;
-        }
-        return theTaxFactor;
-    }
-
-    private double ageBusinessRule(int age) throws InvalidParameterException {
-        double theTaxFactor = 1.0;
-
-        if (age < 0) {
-            throw new InvalidParameterException("age is negative");
-        } else if (age > 70) {
-            theTaxFactor = 0.75;
-        } else if (age > 65) {
-            theTaxFactor = 0.80;
-        }
-        return theTaxFactor;
-    }
-
-    private double genderBusinessRule(Gender gender) throws InvalidParameterException {
-        double theTaxFactor = 1.0;
-
-        if (gender == Gender.MALE) {
-            theTaxFactor = 1.0;
-        } else if (gender == Gender.FEMALE) {
-            theTaxFactor = 0.80;
-        } else{
-            throw new InvalidParameterException("gender is not valid");
-        }
-        return theTaxFactor;
-    }
-
 
 }
