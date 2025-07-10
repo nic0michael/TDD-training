70119d1ac827959ea2d1d522d6287ecb9e96d77d
Nico Michael <nic0michael@users.noreply.github.com>
2025-07-10 09:31:17 +0200

    Update CustomerAdapter.java

--- src/main/java/za/customer/adapters/CustomerAdapter.java
+++ src/main/java/za/customer/adapters/CustomerAdapter.java
@@ -11,26 +11,7 @@ import java.security.InvalidParameterException;
 public class CustomerAdapter {
     public CustomerDto convert(CustomerRequest request) throws InvalidParameterException {
 
-        if (request == null) {
-            throw new InvalidParameterException("request is null");
-        }
-        CustomerDto dto = new CustomerDto();
-        try {
-            dto.setAge(Integer.parseInt(request.getAge()));
-            dto.setIncome(Double.parseDouble(request.getIncome()));
-            dto.setName(request.getName());
-            dto.setCustomerIdentity(request.getCustomerIdentity());
-
-            if("MALE".equalsIgnoreCase(request.getCustGender())) {
-                dto.setCustGender(Gender.MALE);
-            } else if("FEMALE".equalsIgnoreCase(request.getCustGender())) {
-                dto.setCustGender(Gender.FEMALE);
-            }
-
-        }catch (Exception e) {
-            throw new InvalidParameterException(e.getMessage());
-        }
-        return dto;
+        return null;
     }
 
 }
